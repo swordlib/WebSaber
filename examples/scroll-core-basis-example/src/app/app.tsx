cc9fe5d09ea0cf4dd9d501a7e670cc4bc64af458
fallenleavesguy <9261562+fallenleavesguy@users.noreply.github.com>
2023-11-08 13:31:38 +0800

    fix: upgrade nx and create scroll-core-examples

---
+++ examples/scroll-core-basis-example/src/app/app.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.scss';
+
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="scroll-core-basis-example" />
+    </div>
+  );
+}
+
+export default App;
